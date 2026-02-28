1dc99a406d579f7a52f0e5d1c726e716c0e215de
RemboDev <r3mbo.rn@gmail.com>
2026-02-28 17:08:29 -0500

    Forma-controlada

--- projects/buscador-pelicula/src/App.jsx
+++ projects/buscador-pelicula/src/App.jsx
@@ -1,28 +1,28 @@
 import './App.css'
 import { useMovies } from './hooks/useMovies.js'
 import { Movies } from './components/movies.jsx'
-import { useRef } from 'react'
+import { useState } from 'react'
 
 function App() {
 
   const {movies} = useMovies()
-  const inputRef = useRef()
+  const [query,setQuery] = useState('')
 
   const handleSubmit = (event) => {
     event.preventDefault()
-    const {query} = Object.fromEntries(
-      new window.FormData(event.target)
-    )
     console.log({query})
   }
 
+  const handleChange = (event) => {
+    setQuery(event.target.value)
+  }
 
   return (
     <div className='page'>
       <header >
         <h1>Buscador de Peliculas</h1>
         <form className='form' onSubmit={handleSubmit}>
-          <input ref={inputRef} type="text" name="" id="" placeholder='Avengers, Star Wars, Crepusculo' />
+          <input onChange={handleChange} value={query} type="text" name="" id="" placeholder='Avengers, Star Wars, Crepusculo' />
           <button type='submit'>Buscar</button>
         </form>
       </header>
